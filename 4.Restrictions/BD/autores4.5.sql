1834c18713accdba15ea450b4f5bdfe1146272c0
Klvst3r <klvst3r@gmail.com>
2022-12-06 18:07:16 -0600

    Fixing BD autores

--- 4.Restrictions/BD/autores4.5.sql
+++ 4.Restrictions/BD/autores4.5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-12-2022 a las 00:51:21
+-- Tiempo de generación: 07-12-2022 a las 01:06:23
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.1.12
 
@@ -28,28 +28,30 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `autores` (
-  `autor_id` int(11) DEFAULT NULL,
-  `nombre` varchar(20) DEFAULT NULL,
-  `appaterno` varchar(25) DEFAULT NULL,
-  `apmaterno` varchar(25) DEFAULT NULL,
-  `genero` char(1) DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `pais_origen` varchar(40) DEFAULT NULL
+  `autor_id` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `appaterno` varchar(25) NOT NULL,
+  `apmaterno` varchar(25) NOT NULL,
+  `seudonimo` varchar(50) NOT NULL,
+  `genero` char(1) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `pais_origen` varchar(40) NOT NULL,
+  `fecha_creacion` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Volcado de datos para la tabla `autores`
 --
 
-INSERT INTO `autores` (`autor_id`, `nombre`, `appaterno`, `apmaterno`, `genero`, `fecha_nacimiento`, `pais_origen`) VALUES
-(1, 'Test autor_nombre', 'Test_autor_appaterno', 'autor_apmaterno', 'M', '2018-01-03', 'México'),
-(2, 'Segundo Autor', NULL, NULL, NULL, NULL, NULL),
-(3, 'Test tercer_nombre', 'Test_autor_appaterno', 'autor_apmaterno', 'M', '2018-01-03', 'México'),
-(4, 'Test cuarto_nombre', 'Test_autor_appaterno', 'autor_apmaterno', 'M', '2018-01-03', 'México'),
-(5, 'Test quinto_nombre', 'Test_autor_appaterno', 'autor_apmaterno', 'M', '2018-01-03', 'México'),
-(NULL, NULL, NULL, NULL, NULL, '0000-00-00', NULL),
-(0, NULL, NULL, NULL, NULL, NULL, NULL),
-(NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `autores` (`autor_id`, `nombre`, `appaterno`, `apmaterno`, `seudonimo`, `genero`, `fecha_nacimiento`, `pais_origen`, `fecha_creacion`) VALUES
+(1, 'Test autor_nombre', 'Test_autor_appaterno', 'autor_apmaterno', '', 'M', '2018-01-03', 'México', '2022-12-06 18:05:42'),
+(2, 'Segundo Autor', '', '', '', '', '0000-00-00', '', '2022-12-06 18:05:42'),
+(3, 'Test tercer_nombre', 'Test_autor_appaterno', 'autor_apmaterno', '', 'M', '2018-01-03', 'México', '2022-12-06 18:05:42'),
+(4, 'Test cuarto_nombre', 'Test_autor_appaterno', 'autor_apmaterno', '', 'M', '2018-01-03', 'México', '2022-12-06 18:05:42'),
+(5, 'Test quinto_nombre', 'Test_autor_appaterno', 'autor_apmaterno', '', 'M', '2018-01-03', 'México', '2022-12-06 18:05:42'),
+(0, '', '', '', '', '', '0000-00-00', '', '2022-12-06 18:05:42'),
+(0, '', '', '', '', '', '0000-00-00', '', '2022-12-06 18:05:42'),
+(0, '', '', '', '', '', '0000-00-00', '', '2022-12-06 18:05:42');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
